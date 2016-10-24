4eef45ce142fb16592883846216fd1a3335f6310
Ivan Nazarov <ivannnnz@gmail.com>
2016-10-24 15:01:45 +0300

    Initial commit of the wrapper

---
+++ l1tf/_l1tf.pyx
@@ -0,0 +1,74 @@
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: initializedcheck=False
+# cython: nonecheck=False
+
+# Author: Ivan Nazarov <ivannnnz@gmail.com>
+
+import numpy as np
+cimport numpy as np
+cimport cython
+
+from cpython cimport bool
+
+np.import_array()
+
+cdef extern from "l1_tf/l1tf.c":
+    # had to change `lambda` to `C`
+    int c_l1tf "l1tf" (const int n, const double *y, const double C, double *x) nogil
+    double c_l1tf_Cmax "l1tf_lambdamax"(const int n, double *y) nogil
+
+def l1tf(double[::1] X, double C, bool rel_c):
+    """Driver function for L1 trend filtering via primal-dual algorithm by
+    Kwangmoo Koh, Seung-Jean Kim, and Stephen Boyd (C version 0.7 Aug 18 2007,
+    cf. http://stanford.edu/~boyd/l1_tf/).
+
+    Parameters
+    ----------
+    X: pd.Series or pd.DataFrame
+        A 1D or flat data container wit hbuffer interface.
+    C: float
+        The L1 regularization coefficient (`lambda`).
+    rel_c: boolean
+        Determines whether the specified `C` an adaptively chosen maximum.
+
+    Returns
+    -------
+    trend : arraylike
+        The trend inferred by the `l1 trend filtering via primal-dual algorithm`.
+        If `ary` is a Pandas object then its names/columns and index are preserved.
+    """
+
+    cdef double C_max
+    cdef np.intp_t n_samples = X.shape[0]
+    cdef np.double_t[::1] output_ = np.empty(n_samples, dtype=np.double)
+
+    if rel_c:
+        C_max = c_l1tf_Cmax(n_samples, &X[0])
+        if C_max < 0:
+            raise RuntimeError("""Cannot solve for adaptive regularization parameter.""")
+        C *= C_max
+
+    with nogil:
+        c_l1tf(n_samples, &X[0], C, &output_[0])
+
+    return output_
+
+def l1tf_Cmax(double[::1] X):
+    """A helper rotine for adaptive `C` value selection.
+    Parameters
+    ----------
+    X: pd.Series or pd.DataFrame
+        A 1D or flat data container wit hbuffer interface.
+
+    Returns
+    -------
+    C_max : float
+        The largest adaptive `C` value.
+    """
+
+    cdef double C_max = c_l1tf_Cmax(X.shape[0], &X[0])
+    if C_max < 0:
+        raise RuntimeError("""Cannot solve for adaptive regularization parameter.""")
+    return C_max
---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup script for the L1_tf wrapper."""
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+import numpy
+
+install_reqs = parse_requirements("requirements.txt", session=PipSession())
+
+# python setup.py build_ext [--inplace]
+setup(
+    name="l1tf",
+    ext_modules=cythonize([Extension("l1tf/_l1tf", ["l1tf/_l1tf.pyx"],
+                                     include_dirs=[numpy.get_include()],),]),
+    cmdclass={"build_ext": build_ext},
+    packages=["l1tf",],
+    author='Ivan Nazarov',
+    version='0.1.2',
+    description="""A python wrapper for L1 trend filtering via primal-dual algorithm """
+                """by Kwangmoo Koh, Seung-Jean Kim, and Stephen Boyd """
+                """(http://stanford.edu/~boyd/l1_tf/)""",
+    install_requires=[str(ir.req) for ir in install_reqs],
+)
